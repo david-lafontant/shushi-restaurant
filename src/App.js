bb85b7b6803f779ef638cd6539669e7367af2d04
errea <eri-ngozi.okereafor@heroshe.com>
2022-12-07 15:23:26 +0100

    ADD: content js for menu content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
-import Home from './components/home/home';
+import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Menu from './components/Menu';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -9,4 +12,29 @@ function App() {
   );
 }
 
+// function App() {
+//   return (
+//     <>
+//       {/* This is the alias of BrowserRouter i.e. Router */}
+//       <Router>
+//         <Routes>
+//           {/* This route is for home component
+//           with exact path "/", in component props
+//           we passes the imported component */}
+//           <Route exact path="/" component={Home} />
+
+//           {/* This route is for about component
+//           with exact path "/about", in component
+//           props we passes the imported component */}
+//           <Route path="/menu" component={Menu} />
+
+//           {/* If any route mismatches the upper
+//           route endpoints then, redirect triggers
+//           and redirects app to home component with to="/" */}
+//           {/* <Redirect to="/" /> */}
+//         </Routes>
+//       </Router>
+//     </>
+//   );
+// }
 export default App;
---
+++ src/components/content.js
@@ -0,0 +1,28 @@
+const menuData = [
+  {
+    id: 1,
+    name: 'Smashed Avo',
+    image: './assets/images/food1.svg',
+  },
+  {
+    id: 2,
+    name: 'Yin & Yang',
+    image: './assets/images/food1.svg',
+  },
+  {
+    id: 3,
+    name: 'Pancakes',
+    image: './assets/images/food1.svg',
+  },
+  {
+    id: 4,
+    name: 'Huevos Rancheros',
+    image: './assets/images/food1.svg',
+  },
+  {
+    id: 5,
+    name: 'Huevos Rancheros',
+    image: './assets/images/food1.svg',
+  },
+];
+export default menuData;
