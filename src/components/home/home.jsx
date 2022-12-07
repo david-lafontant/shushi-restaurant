fb5135da0d53317fded2970f0d380888dd584358
david-lafontant <lafontant.david@gmail.com>
2022-12-06 19:08:00 -0600

    Add background image

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router';
+// import { Link } from 'react-router-dom';
+import landing from '../../assets/images/landing.svg';
 
 const Home = () => {
   return (
-    <div>
+    <div className="w-full h-screen flex" style={{ backgroundImage: `url(${landing})` }}>
+      <div>Chevron left</div>
       <div>
-        Chevron left
-      </div>
-      <div>
-        <h1>Welcome to Sushi Restaurant</h1>
-        <p>People eat with their eyes and Shushi Restaurant creates an easy way for
-          customers to order when they can see beautiful photos of your food </p>
+        <h1 className="text-3xl font-bold underline">
+          Welcome to Sushi Restaurant
+        </h1>
+        <p>
+          People eat with their eyes and Shushi Restaurant creates an easy way
+          for customers to order when they can see beautiful photos of your food
+        </p>
         <Link to="/about">About</Link>
         <Link to="/menu">Menu</Link>
       </div>
       <div>Chevron right</div>
     </div>
   );
-}
+};
 
 export default Home;
