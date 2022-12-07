ac6ed5087adecaa82e0ba606ea1bdfceb28ab3e9
david-lafontant <lafontant.david@gmail.com>
2022-12-06 19:57:06 -0600

    Fix anchor linter error - center home page content

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -2,24 +2,23 @@ import React from 'react';
 // import { Link } from 'react-router-dom';
 import landing from '../../assets/images/landing.svg';
 
-const Home = () => {
-  return (
-    <div className="w-full h-screen flex" style={{ backgroundImage: `url(${landing})` }}>
-      <div>Chevron left</div>
-      <div>
-        <h1 className="text-3xl font-bold underline">
-          Welcome to Sushi Restaurant
-        </h1>
-        <p>
-          People eat with their eyes and Shushi Restaurant creates an easy way
-          for customers to order when they can see beautiful photos of your food
-        </p>
-        <Link to="/about">About</Link>
-        <Link to="/menu">Menu</Link>
-      </div>
-      <div>Chevron right</div>
+const Home = () => { return (
+  <div className="text-white w-full h-screen flex justify-between items-center" style={{ backgroundImage: `url(${landing})` }}>
+    <div>Chevron left</div>
+    <div>
+      <h1 className="text-3xl font-bold">
+        Welcome to Sushi Restaurant
+      </h1>
+      <p>
+        People eat with their eyes and Shushi Restaurant creates an easy way
+        for customers to order when they can see beautiful photos of your food
+      </p>
+      <a href="https://www.google.com/">About</a>
+      <a href="https://www.google.com/">Menu</a>
     </div>
-  );
+    <div>Chevron right</div>
+  </div>
+);
 };
 
 export default Home;
