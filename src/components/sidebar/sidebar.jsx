7cbca9472479c561f77a661c01b49fc63f4c6ad1
david-lafontant <lafontant.david@gmail.com>
2022-12-07 20:33:31 -0600

    Add on-hover effect

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -9,26 +9,26 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 
 const Sidebar = () => (
-  <div className="bg-black text-white w-1/12 h-4/5 rounded-md shadow-2xl p-2">
+  <div className="bg-black text-white w-1/12 h-4/5 rounded-md shadow-2xl mt-16">
 
-    <ul className="text-4xl flex flex-col items-center h-full justify-evenly">
-      <li>
+    <ul className="text-4xl flex flex-col items-center h-full justify-evenly w-full">
+      <li className="text-beige font-bold antialiased cursor-pointer hover:bg-beige hover:text-black w-full text-center ">
         S.
       </li>
-      <li>
-        <FontAwesomeIcon icon={faHouse} />
+      <li className="antialiased hover:bg-beige hover:text-black w-full text-center ">
+        <FontAwesomeIcon icon={faHouse} className="cursor-pointer" />
       </li>
-      <li>
-        <FontAwesomeIcon icon={faCutlery} />
+      <li className="antialiased hover:bg-beige hover:text-black w-full text-center ">
+        <FontAwesomeIcon icon={faCutlery} className="cursor-pointer" />
       </li>
-      <li>
-        <FontAwesomeIcon icon={faShoppingCart} />
+      <li className="antialiased hover:bg-beige hover:text-black w-full text-center ">
+        <FontAwesomeIcon icon={faShoppingCart} className="cursor-pointer" />
       </li>
-      <li>
-        <FontAwesomeIcon icon={faLocationDot} />
+      <li className="antialiased hover:bg-beige hover:text-black w-full text-center ">
+        <FontAwesomeIcon icon={faLocationDot} className="cursor-pointer" />
       </li>
-      <li>
-        <FontAwesomeIcon icon={faSignOut} />
+      <li className="antialiased hover:bg-beige hover:text-black w-full text-center ">
+        <FontAwesomeIcon icon={faSignOut} className="cursor-pointer" />
       </li>
     </ul>
 
